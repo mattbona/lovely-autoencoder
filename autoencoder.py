36c54b896ea47dc25d4e37bbb20b300711db5930
mattbona <matteo.bonamassa1@gmail.com>
2020-10-21 19:48:35 +0200

    Add run() func

--- autoencoder.py
+++ autoencoder.py
@@ -2,15 +2,14 @@ import src. params as params
 import src.variables as variables
 import src.ae_utils as util
 
-if __name__ == '__main__':
+def run(input_dim, central_hidden_dim, activation_func, bias, loss, opt,
+        learning_rate, momentum, weight_decay, folds_number, epochs_number, batch_dim):
 
-    util.check_dirs()
+    model = util.get_autoencoder(input_dim,central_hidden_dim,activation_func,bias)
+    loss_fn = util.get_loss_function(loss)
+    optimizer = util.get_optimizer(model, opt, learning_rate, momentum, weight_decay)
 
-    model = util.get_autoencoder(params.INPUT_DIMENSION,params.CENTRAL_HIDDEN_DIMENSION,params.ACTIVATION_FUNCTION,params.BIAS)
-    loss_fn = util.get_loss_function(params.LOSS)
-    optimizer = util.get_optimizer(model, params.OPTIMIZER, params.LEARNING_RATE, params.MOMENTUM, params.WEIGHT_DECAY)
-
-    util.train_model_with_external_cross_val(model, loss_fn, optimizer, params.FOLDS_NUMBER, params.EPOCHS_NUMBER, params.BATCH_DIMENSION)
+    util.train_model_with_external_cross_val(model, loss_fn, optimizer, folds_number, epochs_number, batch_dim)
 
     util.write_on_file_average_stddev_losses(params.RESULTS_DIR+'epoch_loss.dat')
 
@@ -20,3 +19,10 @@ if __name__ == '__main__':
     if params.PRINT_MODEL_PARAMETERS == True:
         print('Printing network parameters...')
         util.print_model_parameters(model)
+
+if __name__ == '__main__':
+
+    util.check_dirs()
+    run(params.INPUT_DIMENSION, params.CENTRAL_HIDDEN_DIMENSION, params.ACTIVATION_FUNCTION,
+        params.BIAS, params.LOSS, params.OPTIMIZER, params.LEARNING_RATE, params.MOMENTUM, params.WEIGHT_DECAY,
+        params.FOLDS_NUMBER, params.EPOCHS_NUMBER, params.BATCH_DIMENSION)
